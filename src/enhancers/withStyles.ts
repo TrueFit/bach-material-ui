f685e62e2103600fff75ec61cce2e24e64b6d299
Josh Gretz <jgretz@truefit.io>
2020-03-09 13:25:50 -0400

    update to typescript and v2 of bach

---
+++ src/enhancers/withStyles.ts
@@ -0,0 +1,21 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import {makeStyles, Styles, WithStylesOptions} from '@material-ui/styles';
+import {PROPS, EnhancerContext, EnhancerResult} from '@truefit/bach';
+import {CLASSES} from '../constants';
+
+export default (
+  createStyles: Styles<any, any>,
+  options: WithStylesOptions = {},
+  classesName: string = CLASSES,
+) => ({generateNewVariable}: EnhancerContext): EnhancerResult => {
+  const useStylesAlias = generateNewVariable();
+  const useStyles = makeStyles(createStyles, options);
+
+  return {
+    dependencies: {
+      [useStylesAlias]: useStyles,
+    },
+    initialize: `const ${classesName} = ${useStylesAlias}(${PROPS});`,
+    props: [classesName],
+  };
+};
