0d88c4856890db98fd45e7f9dc280f288e7bdac7
Josh Gretz <jgretz@truefit.io>
2020-06-03 13:22:03 -0400

    add withMediaQuery

--- src/enhancers/index.ts
+++ src/enhancers/index.ts
@@ -1,2 +1,3 @@
 export {default as withStyles} from './withStyles';
 export {default as withTheme} from './withTheme';
+export {default as withMediaQuery} from './withMediaQuery';
---
+++ src/enhancers/withMediaQuery.ts
@@ -0,0 +1,21 @@
+import {EnhancerContext, EnhancerResult} from '@truefit/bach';
+import useMediaQuery, {Options} from '@material-ui/core/useMediaQuery';
+import {Theme} from '@material-ui/core/styles/createMuiTheme';
+
+export default <T>(callbackName: keyof T, fn: (theme: Theme) => string, options: Options = {}) => ({
+  generateNewVariable,
+}: EnhancerContext): EnhancerResult => {
+  const fnAlias = generateNewVariable();
+  const optionsAlias = generateNewVariable();
+
+  return {
+    dependencies: {
+      useMediaQuery,
+
+      [fnAlias]: fn,
+      [optionsAlias]: options,
+    },
+    initialize: `const ${callbackName} = useMediaQuery(${fnAlias}, ${optionsAlias});`,
+    props: [callbackName as string],
+  };
+};
